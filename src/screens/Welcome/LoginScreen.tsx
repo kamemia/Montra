9d7965d45e51750c7b9fa8d7f62334e560ac2852
Vincent Kamemia <kamemia18@yahoo.com>
2024-02-12 12:28:56 +0300

    Changed Launcher Icon

---
+++ src/screens/Welcome/LoginScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { SafeAreaView, View, Text, StyleSheet } from 'react-native'
+
+function LoginScreen() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Login</Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default LoginScreen
